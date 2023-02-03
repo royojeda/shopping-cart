be880357d6a2538a95c0985891f0f62800539d34
Dennis Ojeda <royojeda1@gmail.com>
2023-02-04 05:48:38 +0800

    build: setup the development environment
    This project uses Create React App with TypeScript, Jest, Tailwind CSS,
    ESLint, Prettier, and React Router.

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex min-h-screen w-screen bg-black">
+      <div className="flex flex-1 items-center justify-center text-white">
+        Hello, world!
+      </div>
+    </div>
+  );
+}
