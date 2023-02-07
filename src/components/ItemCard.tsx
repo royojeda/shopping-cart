7b0e3840d767f305f64b18097414c78346f0780c
Dennis Ojeda <royojeda1@gmail.com>
2023-02-07 08:32:32 +0800

    refactor: remove unnecessary key attribute

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -6,7 +6,7 @@ interface ItemCardProps {
 
 export default function ItemCard({ item }: ItemCardProps) {
   return (
-    <div key={item.id} role="listitem" className="flex w-full flex-col gap-2">
+    <div role="listitem" className="flex w-full flex-col gap-2">
       <div className="aspect-square w-full rounded-lg border border-neutral-900 bg-neutral-700 shadow-lg shadow-neutral-900" />
       <div className="text-center">{item.name}</div>
     </div>
