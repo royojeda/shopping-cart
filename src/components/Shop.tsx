dd8f1b9a3dbf8cedfd198e697860b3df8c557547
Dennis Ojeda <royojeda1@gmail.com>
2023-02-10 09:41:09 +0800

    refactor: remove the ItemList component
    ItemCards are now rendered directly from the Shop component.

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -1,25 +1,23 @@
 import { useState } from "react";
-import uniqid from "uniqid";
-import Item from "../types";
+import { CartItem, Item } from "../types";
 import Cart from "./Cart";
-import ItemsList from "./ItemsList";
+import ItemCard from "./ItemCard";
 
 interface ShopProps {
-  allItems: { name: string }[];
+  allItems: Item[];
 }
 
 export default function Shop({ allItems }: ShopProps) {
-  const uniqueItems: Item[] = allItems.map((item) => ({
-    id: uniqid() as string,
-    ...item,
-  }));
-
-  const [cartItems, setCartItems] = useState<Array<Item>>([]);
+  const [cartItems, setCartItems] = useState<Array<CartItem>>([]);
 
   return (
-    <div className="flex w-full flex-col gap-8 p-8">
+    <div className="flex w-full flex-col gap-8 p-4">
       <Cart items={cartItems} />
-      <ItemsList items={uniqueItems} />
+      <div role="list" className="grid w-full grid-cols-1 gap-8">
+        {allItems.map((item) => (
+          <ItemCard key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 }
--- src/tests/Shop.test.tsx
+++ src/tests/Shop.test.tsx
@@ -1,31 +1,32 @@
 import { render } from "@testing-library/react";
-import ItemsList from "../components/ItemsList";
 import Cart from "../components/Cart";
+import ItemCard from "../components/ItemCard";
 import Shop from "../components/Shop";
 
-jest.mock("../components/ItemsList");
-
-jest.mock("uniqid", () => () => "mockId");
+jest.mock("../components/ItemCard");
 
 jest.mock("../components/Cart");
 
-test("renders ItemsList with the correct props", () => {
+test("renders the correct ItemCards", () => {
   const items = [
-    { id: "mockId", name: "Item 1" },
-    { id: "mockId", name: "Item 2" },
-    { id: "mockId", name: "Item 3" },
+    { id: 1, name: "Item 1", description: "This is an item.", price: 1 },
+    { id: 2, name: "Item 2", description: "This is an item.", price: 2 },
+    { id: 3, name: "Item 3", description: "This is an item.", price: 3 },
   ];
 
   render(<Shop allItems={items} />);
 
-  expect(ItemsList).toHaveBeenCalledWith({ items }, {});
+  expect(ItemCard).toHaveBeenCalledTimes(3);
+  expect(ItemCard).toHaveBeenNthCalledWith(1, { item: items[0] }, {});
+  expect(ItemCard).toHaveBeenNthCalledWith(2, { item: items[1] }, {});
+  expect(ItemCard).toHaveBeenNthCalledWith(3, { item: items[2] }, {});
 });
 
 test("renders Cart", () => {
   const items = [
-    { id: "mockId", name: "Item 1" },
-    { id: "mockId", name: "Item 2" },
-    { id: "mockId", name: "Item 3" },
+    { id: 1, name: "Item 1", description: "This is an item.", price: 1 },
+    { id: 2, name: "Item 2", description: "This is an item.", price: 2 },
+    { id: 3, name: "Item 3", description: "This is an item.", price: 3 },
   ];
 
   render(<Shop allItems={items} />);
