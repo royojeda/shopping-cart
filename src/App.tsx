66c1a0799cbffbbb2eb8ae06ec5c7265e1efcf9c
Dennis Ojeda <royojeda1@gmail.com>
2023-02-10 09:39:44 +0800

    refactor: move item id assignment step to App

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,17 @@ import Shop from "./components/Shop";
 import inventory from "./inventory.json";
 
 export default function App() {
+  const uniqueItems = inventory.items.map((item, index) => ({
+    ...item,
+    id: index,
+  }));
+
   return (
     <div className="flex min-h-screen w-screen flex-col">
-      <div className="flex flex-1 items-center justify-center bg-neutral-800 p-4 text-neutral-300">
+      <div className="flex flex-1 items-center justify-center bg-neutral-800 p-4 text-neutral-200">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop allItems={inventory.items} />} />
+          <Route path="/shop" element={<Shop allItems={uniqueItems} />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
