8ef9f051d75c0f0fff97d9060676830e8c7b8bf7
Dennis Ojeda <royojeda1@gmail.com>
2023-02-16 10:57:49 +0800

    fix: prevent app from overflowing the viewport width

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ export default function App() {
   }));
 
   return (
-    <div className="flex min-h-screen w-screen flex-col">
+    <div className="flex min-h-screen max-w-[100vw] flex-col">
       <div className="flex flex-1 items-center justify-center bg-neutral-800 p-4 text-neutral-200">
         <Routes>
           <Route path="/" element={<Home />} />
