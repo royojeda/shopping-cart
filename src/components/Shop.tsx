b35bac7e9b38c96593091d04f2f864e05be1084e
Dennis Ojeda <royojeda1@gmail.com>
2023-02-18 19:17:53 +0800

    feat: link the shopping cart header in shop back to home

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { useState } from "react";
 import { CartItem, Item } from "../types";
 import Cart from "./Cart";
@@ -58,9 +59,12 @@ export default function Shop({ allItems }: ShopProps) {
   return (
     <div className="w-full pt-[104px]">
       <div className="fixed inset-x-0 top-0 flex justify-between bg-neutral-800 p-8 shadow-md shadow-neutral-900">
-        <div className="flex items-center text-2xl font-medium tracking-tight">
+        <Link
+          to="/"
+          className="flex items-center text-2xl font-medium tracking-tight"
+        >
           Shopping Cart
-        </div>
+        </Link>
         <Cart
           totalPrice={cartItems.reduce(
             (total, item) => total + item.price * item.quantity,
