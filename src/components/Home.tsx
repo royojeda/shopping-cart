0f42baca7977b216af8b29056aedb674f070a140
Dennis Ojeda <royojeda1@gmail.com>
2023-02-18 19:14:26 +0800

    feat: add home page background image

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,34 @@
 import { Link } from "react-router-dom";
+import backgroundImage from "../images/home_background.jpg";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-4 text-center text-neutral-300">
-      <h1>Home Page</h1>
-      <Link to="/shop" className="bg-neutral-700 px-4 py-2">
-        Shop now
-      </Link>
-    </div>
+    <>
+      <img
+        src={backgroundImage}
+        alt=""
+        className="fixed flex h-screen w-screen items-center justify-center object-cover brightness-[.4]"
+      />
+      <div className="z-10 flex w-full max-w-[90vw] flex-col items-center gap-8 text-center text-neutral-300">
+        <h1 className="text-6xl font-light">Shopping Cart</h1>
+        <Link
+          to="/shop"
+          className="w-full max-w-xs rounded-lg bg-neutral-700 px-4 py-4 text-2xl shadow-lg shadow-neutral-900 transition hover:bg-neutral-600 active:bg-neutral-900"
+        >
+          Shop now
+        </Link>
+      </div>
+      <div className="fixed inset-x-0 bottom-0 py-2 px-4 text-center text-neutral-400">
+        Background image by{" "}
+        <a
+          href="https://unsplash.com/photos/wvbOWcIDuYg"
+          target="_blank"
+          className="underline"
+          rel="noreferrer"
+        >
+          hosein zanbori at Unsplash
+        </a>
+      </div>
+    </>
   );
 }
